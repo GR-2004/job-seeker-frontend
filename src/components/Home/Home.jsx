e171bd4ed6f0f993bfb6d30a8d7dce9b11948ece
Ganesh Rana <ganeshrana6377772039@gmail.com>
2024-02-28 12:05:09 +0530

    added home page

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,10 +1,24 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { Context } from '../../main'
+import { Navigate } from 'react-router-dom'
+import HeroSection from './HeroSection.jsx'
+import HowitWorks from './HowitWorks.jsx'
+import PopularCategory from './PopularCategory.jsx'
+import PopularCompaines from './PopularCompaines.jsx'
+
 
 const Home = () => {
+  const {isAuthorized} = useContext(Context)
+  if(!isAuthorized){
+    return <Navigate to={'/login'}/>
+  }
   return (
-    <>
-      
-    </>
+    <section className='homePage page'>
+      <HeroSection/>
+      <HowitWorks/>
+      <PopularCategory/>
+      <PopularCompaines/>
+    </section>
   )
 }
 
